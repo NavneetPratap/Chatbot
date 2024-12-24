ed553c2e34e5bd1e78d5732c9ced5adc39391881
NavneetPratap <94733084+NavneetPratap@users.noreply.github.com>
2024-12-24 23:17:16 +0530

    new feature

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request
 import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from googlesearch import search
+from deep_translator import GoogleTranslator
 nltk.download('punkt')
 nltk.download('punkt_tab')
 nltk.download('wordnet')
-from googlesearch import search
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
 
 app = Flask(__name__)
 
 lemmatizer = WordNetLemmatizer()
+translator = GoogleTranslator(source = 'auto', target = 'en')
 
 def preprocess(user_input):
     tokens = word_tokenize(user_input)
@@ -30,24 +32,34 @@ def search_web(query):
 
 def get_response(user_input):
     tokens = preprocess(user_input)
-    if "hi" in tokens or "hello" in tokens:
-        return random.choice(responses["greet"])
-    elif "name" in tokens:
-        return random.choice(responses["ask_name"])
-    elif "bye" in tokens:
-        return random.choice(responses["farewell"])
-    elif "search" in tokens:
-        query = " ".join(tokens[tokens.index("search") + 1:])
-        results = search_web(query)
-        if results:
-            return f"results for '{query}':\n" + "\n".join(results)
+    try:
+        if "hi" in tokens or "hello" in tokens:
+            return random.choice(responses["greet"])
+        elif "name" in tokens:
+            return random.choice(responses["ask_name"])
+        elif "bye" in tokens:
+            return random.choice(responses["farewell"])
+        elif "search" in tokens:
+            query = " ".join(tokens[tokens.index("search") + 1:])
+            results = search_web(query)
+            if results:
+                return f"results for '{query}':\n" + "\n".join(results)
+            else:
+                return "Cannot find anything!"
+        elif "translate" in tokens:
+            text_to_translate = " ".join(tokens[1:])
+            translation = translator.translate(text_to_translate, dest = 'en')
+            translated_text = translator.translate(text_to_translate)
+            return f"Original : {text_to_translate}\nTranslation(en): {translated_text}"    
         else:
-            return "Cannot find anything!"
-    else:
-        return "Write it in different way!"
+            return "Write it in different way!"
+    except Exception as e:
+        return f"An error occured: {e}"
+    
 @app.route("/")
 def home():
     return render_template("index.html")
+
 @app.route("/get")
 def get_bot_response():
     user_input = request.args.get("msg")
